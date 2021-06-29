38817084419dd9e1b34eaa80506e8bc53bb54c28
Brian Moakley <brian.douglas.moakley@gmail.com>
2021-06-29 15:17:55 -0400

    Adds sample projects

---
+++ 02-manage-transactions/Starter/InsomniOwl/InsomniOwl/Model/IAPStore.swift
@@ -0,0 +1,133 @@
+///// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+import StoreKit
+import SwiftKeychainWrapper
+
+class IAPStore: NSObject, ObservableObject {
+
+  private let productIdentifiers: Set<String>
+  public private(set) var purchasedProducts = Set<String>()
+  @Published private(set) var products = [Product]()
+
+  init(productsIDs: Set<String>) {
+    productIdentifiers = productsIDs
+    purchasedProducts = Set(productIdentifiers.filter { KeychainWrapper.standard.bool(forKey: $0) ?? false })
+    super.init()
+  }
+
+  @MainActor func requestProducts() async {
+    do {
+      products = try await Product.products(for: productIdentifiers)
+      objectWillChange.send()
+    } catch {
+        print(error.localizedDescription)
+    }
+  }
+
+  func buyProduct(product: Product) async {
+    var transaction: Transaction? = nil
+    var verified = false
+    do {
+      let payment = try await product.purchase()
+      switch payment {
+      case .success(let verification):
+        switch verification {
+            case .verified(let succesTransaction):
+                transaction = succesTransaction
+                verified = true
+            case .unverified(let failedTransaction):
+                // run my own verification method
+                print(failedTransaction.jwsRepresentation)
+                print(failedTransaction.headerData)
+                print(failedTransaction.payloadData)
+                print(failedTransaction.signedData)
+        }
+      case .userCancelled, .pending:
+          return
+      default:
+          return
+      }
+    } catch {
+        print(error.localizedDescription)
+    }
+    if verified {
+      guard let transaction = transaction else {
+          return
+      }
+      if product.type == .consumable {
+          await addConsumable(productIdentifier: product.id, amount: 3)
+      } else {
+          await addPurchase(purchaseIdentifier: product.id)
+      }
+      await transaction.finish()
+    }
+  }
+
+    @MainActor func addPurchase(purchaseIdentifier: String) {
+    if productIdentifiers.contains(purchaseIdentifier) {
+      purchasedProducts.insert(purchaseIdentifier)
+      KeychainWrapper.standard.set(true, forKey: purchaseIdentifier)
+      objectWillChange.send()
+    }
+  }
+
+  func isPurchased(_ productIdentifier: String) -> Bool {
+    purchasedProducts.contains(productIdentifier)
+  }
+
+  func restorePurchases() {
+    SKPaymentQueue.default().restoreCompletedTransactions()
+  }
+
+  @MainActor func addConsumable(productIdentifier: String, amount: Int) {
+    var currentTotal = consumableAmountFor(productIdentifier: productIdentifier)
+    currentTotal += amount
+    KeychainWrapper.standard.set(currentTotal, forKey: productIdentifier)
+    objectWillChange.send()
+  }
+
+  func consumableAmountFor(productIdentifier: String) -> Int {
+    KeychainWrapper.standard.integer(forKey: productIdentifier) ?? 0
+  }
+
+    @MainActor func decrementConsumable(productIdentifier: String) {
+    var currentTotal = consumableAmountFor(productIdentifier: productIdentifier)
+    if currentTotal > 0 {
+      currentTotal -= 1
+    }
+    KeychainWrapper.standard.set(currentTotal, forKey: productIdentifier)
+    objectWillChange.send()
+  }
+}
+
